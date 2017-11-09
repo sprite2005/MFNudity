9b1bcba6479fee9d65d92d7f35869c19687f158a
arpandesai <arpandesai@me.com>
2017-11-09 14:30:40 +0530

    initial project add

--- MFNudityExample/ViewController.swift
+++ MFNudityExample/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import MFNudity
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var resultLbl: UILabel!
+    
+    
+    @IBOutlet var resultImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let image = UIImage(named:"burkha.jpg")
+        resultImage.image = image
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func resultAction(_ sender: Any) {
+        
+        MFNudity.shared.checkImageNudity(image: resultImage.image!) { (error, confidence) in
+            
+            if error != nil
+            {
+                let converted = String(format: "%.2f", confidence!)
+                resultLbl.text = "NSFW - \(converted) %"
+            }
 
+        }
+        
+        
+    }
+    
+    
 
 }
 
